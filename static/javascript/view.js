98735ad42f270fb1c048f16539fc8c66c7957705
Michael Fischer <doptrois@gmail.com>
2018-06-10 20:19:02 +0200

    added methods for style switcher, finish state, expand/collapse note, hideNote, fetch sorted notes.

--- static/javascript/view.js
+++ static/javascript/view.js
@@ -1,8 +1,43 @@
-/* eslint-disable */
-import Handlebars from '../vendor/handlebars/handlebars.min.js';
-
 export default class View {
-    constructor(sth) {
-        this.sth = sth;
+    constructor(model) {
+        this.model = model;
+        this.templates = {
+            note: '../hbs-templates/note.hbs',
+            editNote: '../hbs-templates/edit-note.hbs',
+            sort: '../hbs-templates/sort.hbs',
+        };
+        document.querySelector('.container').classList.add(`container--${this.model.userSettings.currentStyle}`);
+    }
+
+    // Style switcher
+    changeCurrentStyle() {
+        const style = this.model.userSettings.currentStyle === 'black' ? 'white' : 'black';
+        document
+            .querySelector('.container')
+            .classList
+            .replace(`container--${this.model.userSettings.currentStyle}`, `container--${style}`);
+    }
+
+    changeFinishState(id) {
+        const note = document.querySelector(`[data-note-id="${id}"]`);
+        note.classList.toggle('note--done');
+        note.querySelector('.note__interactions-item--check').classList.toggle('note__label--checked');
+    }
+
+    expandCollapsNote(id) {
+        const note = document.querySelector(`[data-note-id="${id}"]`);
+        note.querySelector('.note__title').classList.toggle('note__title--expanded');
+        note.querySelector('.note__paragraph').classList.toggle('note__paragraph--expanded');
+    }
+
+    hideNote(id) {
+        const note = document.querySelector(`[data-note-id="${id}"]`);
+        note.classList.toggle('note--deleted');
+    }
+
+    updateShownNotes(notes) {
+        fetch(this.templates.note)
+            .then(response => response.text())
+            .then(data => console.log(Handlebars.compile(data)({ notes })));
     }
 }
