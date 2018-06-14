535854b3c283eaccd7ec7d59e56e0b805704765d
Michael Fischer <doptrois@gmail.com>
2018-06-14 20:19:04 +0200

    removed dublicate event listeners

--- static/javascript/controller.js
+++ static/javascript/controller.js
@@ -3,46 +3,36 @@ export default class Controller {
         this.model = model;
         this.view = view;
 
-        // Event handlers
         this.onSortOptionChange(this.model.userSettings.sortOption);
         if (this.model.userSettings.showFinished) {
             this.view.markUnmarkShowFinished();
         }
 
-        // Style switcher
-        document.body.addEventListener('click', (event) => {
-            const elem = event.target.closest('.button--switcher');
-            if (elem) this.onCurrentStyleChange();
-        });
+        // Event handlers
+        document.body.addEventListener('click', (e) => {
+            // Style switcher
+            const switcher = e.target.closest('.button--switcher');
+            if (switcher) this.onCurrentStyleChange();
 
-        // Finish/Unfinish note
-        document.body.addEventListener('click', (event) => {
-            const elem = event.target.closest('.note__interactions-item--check');
-            if (elem) this.onFinishUnfinishNote(+elem.closest('.note').dataset.noteId);
-        });
+            // Finish/Unfinish note
+            const finish = e.target.closest('.note__interactions-item--check');
+            if (finish) this.onFinishUnfinishNote(+finish.closest('.note').dataset.noteId);
 
-        // Delete note
-        document.body.addEventListener('click', (event) => {
-            const elem = event.target.closest('.note__interactions-item--delete');
-            if (elem) this.onDeleteNote(+elem.closest('.note').dataset.noteId);
-        });
+            // Delete note
+            const del = e.target.closest('.note__interactions-item--delete');
+            if (del) this.onDeleteNote(+del.closest('.note').dataset.noteId);
 
-        // Expand/Collapse note
-        document.body.addEventListener('click', (event) => {
-            const elem = event.target.closest('.note__content');
-            if (elem) this.onExpandCollapsNote(+elem.closest('.note').dataset.noteId);
-        });
+            // Expand/Collapse note
+            const cont = e.target.closest('.note__content');
+            if (cont) this.onExpandCollapsNote(+cont.closest('.note').dataset.noteId);
 
-        // Sort notes
-        document.body.addEventListener('click', (event) => {
-            const elem = event.target.closest('[data-notes-sort]');
-            if (elem) this.onSortOptionChange(elem.dataset.notesSort);
-        });
+            // Sort notes
+            const sort = e.target.closest('[data-notes-sort]');
+            if (sort) this.onSortOptionChange(sort.dataset.notesSort);
 
-        // Show/Hide finished
-        document.body.addEventListener('click', (event) => {
-            const elem = event.target.closest('[data-notes-show-finished]');
-            if (elem) this.onShowFinishedChange();
+            // Show/Hide finished
+            const showHide = e.target.closest('[data-notes-show-finished]');
+            if (showHide) this.onShowFinishedChange();
         });
     }
 
