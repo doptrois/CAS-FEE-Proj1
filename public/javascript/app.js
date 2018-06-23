90da18901e5d0800d61562d292a0f62e0f1f78d9
Michael Fischer <doptrois@gmail.com>
2018-06-23 10:49:11 +0200

    prepared storage handler

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,9 +1,11 @@
+import Storage from './storage.js';
 import Model from './model.js';
 import View from './view.js';
 import Controller from './controller.js';
 
 (() => {
-    const model = new Model();
+    const storage = new Storage();
+    const model = new Model(storage);
     const view = new View(model);
     const controller = new Controller(model, view); // eslint-disable-line no-unused-vars
 })();
